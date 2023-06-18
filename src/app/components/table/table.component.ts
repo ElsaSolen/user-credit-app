b6ea97cffed49dcb842562034276ba1115863c69
ElsaSolen <97947759+ElsaSolen@users.noreply.github.com>
2023-06-18 23:47:37 +0100

    add sorting first attempt

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -20,4 +20,33 @@ export class TableComponent implements OnInit {
     this.displayData = paginatedData;
     this.cdr.detectChanges();
   }
+
+  ascCredits = false;
+  ascUsers = false;
+
+  sort(header) {
+    let arr = [];
+    if (header === 'credits') {
+      arr = this.rawData.slice().sort((a, b) => {
+        return this.ascCredits ? b.credit - a.credit : a.credit - b.credit;
+      });
+      this.ascCredits = !this.ascCredits;
+    } else {
+      arr = this.rawData.slice().sort((a, b) => {
+        const userA = a.user.toUpperCase();
+        const userB = b.user.toUpperCase();
+
+        if (userA < userB) {
+          return this.ascUsers ? 1 : -1;
+        }
+        if (userA > userB) {
+          return this.ascUsers ? -1 : 1;
+        }
+        return 0;
+      });
+    }
+
+    this.ascUsers = !this.ascUsers;
+    this.rawData = arr;
+  }
 }
